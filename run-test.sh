463c0d49077d8fb0f19b32db9d123f9987d74172
plasma147 <plasma147@hotmail.com>
2016-04-07 08:55:19 +0100

    tidying up script

--- run-test.sh
+++ run-test.sh
@@ -2,9 +2,8 @@
 
 if [[ $1 == "-b" ]]; then
   mvn clean install
-  cp ./target/jnomon-0.0.1-SNAPSHOT.jar ./test/
 fi
 
 STEP_7_LENGTH=1234
 
-java -jar ./test/test-0.0.1-SNAPSHOT.jar $STEP_7_LENGTH | java -jar ./test/jnomon-0.0.1-SNAPSHOT.jar -h 2500 -m 1500
+java -jar ./test/test-0.0.1-SNAPSHOT.jar $STEP_7_LENGTH | ./jnomon -h 2500 -m 1500
