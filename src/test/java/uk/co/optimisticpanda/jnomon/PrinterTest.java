bb51ce8c0241246175c39173092b3c05c1eb6b48
plasma147 <plasma147@hotmail.com>
2016-04-18 23:48:34 +0100

    Exclude generated classes from code coverage. Add printer test

---
+++ src/test/java/uk/co/optimisticpanda/jnomon/PrinterTest.java
@@ -0,0 +1,55 @@
+package uk.co.optimisticpanda.jnomon;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import rx.subjects.PublishSubject;
+import uk.co.optimisticpanda.jnomon.Step.QuitStep;
+import uk.co.optimisticpanda.jnomon.formatter.EventListenerAdapter;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PrinterTest {
+
+    @Mock private EventListenerAdapter eventListener;
+    private PublishSubject<Integer> stopper;
+    private Printer printer;
+
+    @Before
+    public void setUp() throws Exception {
+        stopper = PublishSubject.create();
+        printer = new Printer(stopper, eventListener);
+    }
+
+    @Test
+    public void onBeforeCalled() {
+        verify(eventListener).onBeforeAll();
+    }
+    
+    @Test
+    public void quitStepPublishesStopEvent() {
+        printer.call(new QuitStep());
+        
+        verify(eventListener).onFinally(any(), any());
+        assertThat(stopper.hasCompleted()).isTrue();
+    }
+
+    @Test
+    public void handlesTickEvents() {
+        printer.call(new TickStepImpl(1L));
+        verify(eventListener).onUpdate(any(), any());
+    }
+
+    @Test
+    public void handlesLineEvents() {
+        printer.call(new LineStepImpl("A line"));
+        verify(eventListener).onLineStart(any(), any(), eq("A line"));
+    }
+}
